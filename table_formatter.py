b2f15e15c0ec2dda8adcfb9a32187c605dbf65d0
Devansh Dalal <devansh.dalal@thoughtspot.com>
2020-05-06 17:00:29 +0530

    Fix the funds table schema

--- table_formatter.py
+++ table_formatter.py
@@ -6,8 +6,10 @@ def FormatTable(header, table_rows):
 	new_header = {'name': 'weight'}
 	new_table = []
 	for row in table_rows:
-		new_table.append({row[0]:
-				round(Decimal(row[3].replace('%', '')) / Decimal(100.0), 4)})
+		new_table.append({
+			'stock': row[0],
+			'wt': round(Decimal(row[3].replace('%', '')) / Decimal(100.0), 4)}
+		)
 
 	return new_header, new_table
 
